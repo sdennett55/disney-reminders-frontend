196b218a54c5e8a6f93fb6d293dcf1bd5a84be3b
sdennett55@gmail.com <sdennett55@gmail.com>
2020-03-01 21:40:30 -0500

    formatting time better

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function handleDateChange({ value, setSelectedDate, daysToFastPass, setDiningDat
 }
 
 function getTime(date) {
-  return date.toLocaleTimeString();
+  return date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}).replace(/^0+/, '');
 }
 
 function getFormattedDate(date) {
@@ -97,40 +97,49 @@ function App() {
     <Box className="App" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
       <CssBaseline />
       <Box className="App-inner" display="flex" flexDirection="column" alignItems="center" p={4} m={4}>
-        <h1 className="App-title">Get email reminders for your next Walt Disney World vacation!</h1>
-        <form onSubmit={e => handleSubmit({ e, email, diningDate, fastPassDate, setValidationMessage, daysToFastPass })}>
-          <Box mt={1} mb={1}>
-            <TextField helperText={validationMessage && validationMessage.toLowerCase().includes('email') && validationMessage} error={Boolean(validationMessage) && validationMessage.toLowerCase().includes('email')} fullWidth label="Email" type="email" required onChange={e => setEmail(e.target.value)} />
-          </Box>
-          <Box mt={3} mb={2}>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <DatePicker helperText={validationMessage && validationMessage.toLowerCase().includes('date') && validationMessage} error={Boolean(validationMessage) && validationMessage.toLowerCase().includes('date')} label="Day of Arrival" value={selectedDate} fullWidth variant="filled" color="primary" format="MM/dd/yyyy" required onChange={value => handleDateChange({ value, setSelectedDate, daysToFastPass, setDiningDate, setFastPassDate })} />
-            </MuiPickersUtilsProvider>
-          </Box>
-          <Box display="flex" alignItems="center" mt={2} mb={2}>
-            <Checkbox id="isDisneyProperty" type="checkbox" checked={isDisneyProperty} onChange={e => setIsDisneyProperty(!isDisneyProperty)} />
-            <FormLabel htmlFor="isDisneyProperty" size="small">
-              I am staying on Disney property 
-            </FormLabel>
-            <IconButton onClick={e => { setAnchorEl(e.currentTarget) }}>
-              <InfoIcon />
-            </IconButton>
-            <Popover open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={() => { setAnchorEl(null) }}>
-              <Box m={2}>
-                See list of official <Link target="_blank" href="https://disneyworld.disney.go.com/faq/resorts/resort-hotel-list/">Walt Disney World Resorts</Link>
+        {validationMessage === 'Success!' ? (
+          <>
+            <h1 className="App-title">Thank you! We've sent you a confirmation email.</h1>
+            <Button variant="contained" color="primary" type="submit" onClick={() => { setValidationMessage('') }}>Add another Email</Button>
+          </>
+        ) : (
+          <>
+            <h1 className="App-title">Get email reminders for your next Walt Disney World vacation!</h1>
+            <form onSubmit={e => handleSubmit({ e, email, diningDate, fastPassDate, setValidationMessage, daysToFastPass })}>
+              <Box mt={1} mb={1}>
+                <TextField helperText={validationMessage && validationMessage.toLowerCase().includes('email') && validationMessage} error={Boolean(validationMessage) && validationMessage.toLowerCase().includes('email')} fullWidth label="Email" type="email" required onChange={e => setEmail(e.target.value)} />
               </Box>
-            </Popover>
-          </Box>
-          <Box display="flex" justifyContent="center" mt={1} mb={1}>
-            <Button variant="contained" color="primary" type="submit">Remind Me!</Button>
-          </Box>
-        </form>
-        <Box className="App-resultLine" mt={4} mb={2}>
-          ADR Reservations ({DAYS_TO_DINING} Days out): {diningDate && getDateInfo({date: diningDate, type: 'dining'})}
-        </Box>
-        <div className="App-resultLine">
-          FP+ Reservations ({daysToFastPass} Days out): {fastPassDate && getDateInfo({date: fastPassDate, type: 'fastPass'})}
-        </div>
+              <Box mt={2} mb={2}>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <DatePicker helperText={validationMessage && validationMessage.toLowerCase().includes('date') && validationMessage} error={Boolean(validationMessage) && validationMessage.toLowerCase().includes('date')} label="Day of Arrival" value={selectedDate || null} fullWidth variant="filled" color="primary" format="MM/dd/yyyy" required onChange={value => handleDateChange({ value, setSelectedDate, daysToFastPass, setDiningDate, setFastPassDate })} />
+                </MuiPickersUtilsProvider>
+              </Box>
+              <Box display="flex" alignItems="center" mt={2} mb={2}>
+                <Checkbox id="isDisneyProperty" type="checkbox" checked={isDisneyProperty} onChange={e => setIsDisneyProperty(!isDisneyProperty)} />
+                <FormLabel htmlFor="isDisneyProperty" size="small">
+                  I am staying on Disney property 
+                </FormLabel>
+                <IconButton onClick={e => { setAnchorEl(e.currentTarget) }}>
+                  <InfoIcon />
+                </IconButton>
+                <Popover open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={() => { setAnchorEl(null) }}>
+                  <Box m={2}>
+                    See list of official <Link target="_blank" href="https://disneyworld.disney.go.com/faq/resorts/resort-hotel-list/">Walt Disney World Resorts</Link>
+                  </Box>
+                </Popover>
+              </Box>
+              <Box display="flex" justifyContent="center" mt={1} mb={1}>
+                <Button variant="contained" color="primary" type="submit">Remind Me!</Button>
+              </Box>
+            </form>
+            <Box className="App-resultLine" mt={4} mb={2}>
+              ADR Reservations ({DAYS_TO_DINING} Days out): {diningDate && getDateInfo({date: diningDate, type: 'dining'})}
+            </Box>
+            <div className="App-resultLine">
+              FP+ Reservations ({daysToFastPass} Days out): {fastPassDate && getDateInfo({date: fastPassDate, type: 'fastPass'})}
+            </div>
+          </>
+        )}
       </Box>
     </Box>
     </ThemeProvider>
