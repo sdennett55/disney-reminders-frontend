f9458503b1fc194399458eed3c59523efb8c9635
sdennett55@gmail.com <sdennett55@gmail.com>
2020-03-01 21:10:24 -0500

    design makeover, mvp looks good

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
+import { Box, Button, Checkbox, FormLabel, Link, TextField, Popover, IconButton } from '@material-ui/core';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import InfoIcon from '@material-ui/icons/Info';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {
+  MuiPickersUtilsProvider,
+  DatePicker,
+} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 import './App.css';
 
-const DAYS_TO_DINING = 60;
-const DAYS_TO_FASTPASS = 30;
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#0093c4' },
+  },
+});
 
-const getDate = ({date, numOfDays}) => {
+const DAYS_TO_DINING = 180;
+const DAYS_TO_FASTPASS = 30;
+const DAYS_TO_FASTPASS_ON_PROPERTY = 60;
+
+const getDate = ({ date, numOfDays }) => {
   var tempDate = new Date(date);
-  return new Date(tempDate.setDate(tempDate.getDate() - numOfDays)).toISOString().split('T')[0]; 
+  return new Date(tempDate.setDate(tempDate.getDate() - numOfDays));
 }
 
-function handleSubmit({ e, email, date, setValidationMessage, daysToFastPass }) {
+function handleSubmit({ e, email, diningDate, fastPassDate, setValidationMessage }) {
   e.preventDefault();
-
-  const diningDate = getDate({date, numOfDays: DAYS_TO_DINING});
-  const fastPassDate = getDate({date, numOfDays: daysToFastPass});
 
   const user = {
     email,
@@ -28,39 +41,99 @@ function handleSubmit({ e, email, date, setValidationMessage, daysToFastPass }) 
   });
 }
 
+function handleDateChange({ value, setSelectedDate, daysToFastPass, setDiningDate, setFastPassDate }) {
+  if (String(new Date(value)) === 'Invalid Date') {
+    return;
+  }
+
+  const date = new Date(value);
+  date.setHours(6, 0, 0, 0);
+  
+  setSelectedDate(date);
+
+  setDiningDate(getDate({ date, numOfDays: DAYS_TO_DINING }));
+  setFastPassDate(getDate({ date, numOfDays: daysToFastPass }));
+}
+
+function getTime(date) {
+  return date.toLocaleTimeString();
+}
+
+function getFormattedDate(date) {
+  return date.toLocaleDateString();
+}
+
+function getMinDate(daysToFastPass) {
+  const MIN_NUM_OF_DAYS = daysToFastPass ? daysToFastPass : DAYS_TO_FASTPASS;
+  var today = new Date();
+  return new Date(today.setDate(today.getDate() + MIN_NUM_OF_DAYS)).toISOString().split('T')[0];
+}
+
+function getDateInfo({date, type}) {
+  const href = type === 'dining' ? 'https://disneyworld.disney.go.com/dining' : 'https://disneyworld.disney.go.com/fastpass-plus/select-party/';
+
+  if (new Date() > date) {
+    return (
+      <>
+        You can <Link target="_blank" color="secondary" href={href}>make these reservations today!</Link>
+      </>
+    );
+  }
+  return <strong>{getFormattedDate(date)} at {getTime(date)}</strong>;
+}
+
 function App() {
   const [email, setEmail] = useState('');
-  const [date, setDate] = useState('');
+  const [selectedDate, setSelectedDate] = useState('');
+  const [diningDate, setDiningDate] = useState('');
+  const [fastPassDate, setFastPassDate] = useState('');
   const [isDisneyProperty, setIsDisneyProperty] = useState(true);
   const [validationMessage, setValidationMessage] = useState('');
-  const daysToFastPass = isDisneyProperty ? DAYS_TO_DINING : DAYS_TO_FASTPASS;
-  const color = validationMessage === 'Success!' ? 'green' : 'red';
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const daysToFastPass = isDisneyProperty ? DAYS_TO_FASTPASS_ON_PROPERTY : DAYS_TO_FASTPASS;
 
   return (
-    <>
-      <form onSubmit={e => handleSubmit({ e, email, date, setValidationMessage, daysToFastPass })}>
-        <label htmlFor="email">
-          Email
-      </label>
-        <input id="email" type="email" required onChange={e => setEmail(e.target.value)} />
-        <label htmlFor="email">
-          Day of Arrival at Walt Disney World
-      </label>
-        <input id="date" type="date" required onChange={e => setDate(e.target.value)} />
-        <label htmlFor="isDisneyProperty">
-          I am staying on Disney property
-      </label>
-        <input id="isDisneyProperty" type="checkbox" checked={isDisneyProperty} onChange={e => setIsDisneyProperty(!isDisneyProperty)} />
-        <button>Submit</button>
-        {validationMessage && <p style={{color}}>{validationMessage}</p>}
-      </form>
-      <div style={{marginTop: '2em'}}>
-        Dining Reservations (60 Days out)
-      </div>
-      <div>
-        FastPass+ Reservations ({daysToFastPass} Days out)
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+    <Box className="App" display="flex" flexDirection="column" justifyContent="center" alignItems="center">
+      <CssBaseline />
+      <Box className="App-inner" display="flex" flexDirection="column" alignItems="center" p={4} m={4}>
+        <h1 className="App-title">Get email reminders for your next Walt Disney World vacation!</h1>
+        <form onSubmit={e => handleSubmit({ e, email, diningDate, fastPassDate, setValidationMessage, daysToFastPass })}>
+          <Box mt={1} mb={1}>
+            <TextField helperText={validationMessage && validationMessage.toLowerCase().includes('email') && validationMessage} error={Boolean(validationMessage) && validationMessage.toLowerCase().includes('email')} fullWidth label="Email" type="email" required onChange={e => setEmail(e.target.value)} />
+          </Box>
+          <Box mt={3} mb={2}>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DatePicker helperText={validationMessage && validationMessage.toLowerCase().includes('date') && validationMessage} error={Boolean(validationMessage) && validationMessage.toLowerCase().includes('date')} label="Day of Arrival" value={selectedDate} fullWidth variant="filled" color="primary" format="MM/dd/yyyy" required onChange={value => handleDateChange({ value, setSelectedDate, daysToFastPass, setDiningDate, setFastPassDate })} />
+            </MuiPickersUtilsProvider>
+          </Box>
+          <Box display="flex" alignItems="center" mt={2} mb={2}>
+            <Checkbox id="isDisneyProperty" type="checkbox" checked={isDisneyProperty} onChange={e => setIsDisneyProperty(!isDisneyProperty)} />
+            <FormLabel htmlFor="isDisneyProperty" size="small">
+              I am staying on Disney property 
+            </FormLabel>
+            <IconButton onClick={e => { setAnchorEl(e.currentTarget) }}>
+              <InfoIcon />
+            </IconButton>
+            <Popover open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={() => { setAnchorEl(null) }}>
+              <Box m={2}>
+                See list of official <Link target="_blank" href="https://disneyworld.disney.go.com/faq/resorts/resort-hotel-list/">Walt Disney World Resorts</Link>
+              </Box>
+            </Popover>
+          </Box>
+          <Box display="flex" justifyContent="center" mt={1} mb={1}>
+            <Button variant="contained" color="primary" type="submit">Remind Me!</Button>
+          </Box>
+        </form>
+        <Box className="App-resultLine" mt={4} mb={2}>
+          ADR Reservations ({DAYS_TO_DINING} Days out): {diningDate && getDateInfo({date: diningDate, type: 'dining'})}
+        </Box>
+        <div className="App-resultLine">
+          FP+ Reservations ({daysToFastPass} Days out): {fastPassDate && getDateInfo({date: fastPassDate, type: 'fastPass'})}
+        </div>
+      </Box>
+    </Box>
+    </ThemeProvider>
   );
 }
 
